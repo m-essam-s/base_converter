a44e648def67893a5e3cf5279111236ca4cc0113
m-essam-s <sir.m.essam.g@gmail.com>
2023-11-20 17:19:48 +0200

    Main

--- Main.java
+++ Main.java
@@ -3,21 +3,24 @@
  */
 public class Main {
   public static void main(String[] args) {
-    // new MainFrame();
-    System.out.println(Decimal.ToOctal("5.5"));
-    System.out.println(Decimal.ToBinary("5.5"));
-    System.out.println(Decimal.ToHexadecimal("5.5"));
-    System.out.println();
-    System.out.println(Binary.ToDecimal("101.1"));
-    System.out.println(Binary.ToOctal("101.1"));
-    System.out.println(Binary.ToHexadecimal("101.1"));
-    System.out.println();
-    System.out.println(Octal.ToDecimal("5.5"));
-    System.out.println(Octal.ToBinary("5.5"));
-    System.out.println(Octal.ToHexadecimal("5.5"));
-    System.out.println();
-    System.out.println(HexaDecimal.ToDecimal("5.5"));
-    System.out.println(HexaDecimal.ToBinary("5.5"));
-    System.out.println(HexaDecimal.ToOctal("5.5"));
+    new MainFrame();
   }
+  // public static void main(String[] args) {
+  //   new MainFrame();
+  //   // System.out.println(Decimal.ToOctal("5.5"));
+  //   // System.out.println(Decimal.ToBinary("5.5"));
+  //   // System.out.println(Decimal.ToHexadecimal("5.5"));
+  //   // System.out.println();
+  //   // System.out.println(Binary.ToDecimal("101.1"));
+  //   // System.out.println(Binary.ToOctal("101.1"));
+  //   // System.out.println(Binary.ToHexadecimal("101.1"));
+  //   // System.out.println();
+  //   // System.out.println(Octal.ToDecimal("5.5"));
+  //   // System.out.println(Octal.ToBinary("5.5"));
+  //   // System.out.println(Octal.ToHexadecimal("5.5"));
+  //   // System.out.println();
+  //   // System.out.println(HexaDecimal.ToDecimal("5.5"));
+  //   // System.out.println(HexaDecimal.ToBinary("5.5"));
+  //   // System.out.println(HexaDecimal.ToOctal("5.5"));
+  // }
 }
