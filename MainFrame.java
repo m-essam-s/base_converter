837ba97a73f1b62d448226bcff339eab86eda345
m-essam-s <sir.m.essam.g@gmail.com>
2023-11-20 12:29:49 +0200

    GUI MainFrame

--- MainFrame.java
+++ MainFrame.java
@@ -1,34 +1,34 @@
 import javax.swing.JFrame;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
+// import javax.swing.JTextField;
+// import javax.swing.JButton;
+// import javax.swing.JComboBox;
 
 public class MainFrame extends JFrame {
-  private JTextField textField;
-  private JButton button;
-  private JComboBox<String> comboBox;
+  // private JTextField textField;
+  // private JButton button;
+  // private JComboBox<String> comboBox;
 
-  public MainFrame() {
-    super("Number System Converter");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setSize(500, 500);
-    setLayout(null); // Use null layout for manual component placement
-    initComponents();
-    setVisible(true);
-  }
+  // public MainFrame() {
+  //   super("Number System Converter");
+  //   setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  //   setSize(500, 500);
+  //   setLayout(null); // Use null layout for manual component placement
+  //   initComponents();
+  //   setVisible(true);
+  // }
 
-  private void initComponents() {
-    textField = new JTextField();
-    textField.setBounds(50, 50, 200, 30); // Set the size and position of the text field
-    add(textField);
+  // private void initComponents() {
+  //   textField = new JTextField();
+  //   textField.setBounds(50, 50, 200, 30); // Set the size and position of the text field
+  //   add(textField);
 
-    button = new JButton("Convert");
-    button.setBounds(50, 100, 100, 30); // Set the size and position of the button
-    add(button);
+  //   button = new JButton("Convert");
+  //   button.setBounds(50, 100, 100, 30); // Set the size and position of the button
+  //   add(button);
 
-    String[] options = { "Option 1", "Option 2", "Option 3" };
-    comboBox = new JComboBox<>(options);
-    comboBox.setBounds(50, 150, 150, 30); // Set the size and position of the combo box
-    add(comboBox);
-  }
+  //   String[] options = { "Option 1", "Option 2", "Option 3" };
+  //   comboBox = new JComboBox<>(options);
+  //   comboBox.setBounds(50, 150, 150, 30); // Set the size and position of the combo box
+  //   add(comboBox);
+  // }
 }
