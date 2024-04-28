791f8f769d223a167773eb3699fbc8ee6148b69c
m-essam-s <sir.m.essam.g@gmail.com>
2024-04-28 12:06:45 +0300

    Updates

--- app/src/main/java/numbersystemconverter/Binary.java
+++ app/src/main/java/numbersystemconverter/Binary.java
@@ -1,4 +1,6 @@
- /**
+package numbersystemconverter;
+
+/**
  * Binary
  */
 public class Binary {
@@ -7,7 +9,7 @@ public class Binary {
     if (binary.contains(".")) {
       String FractionPart = binary.substring(binary.indexOf('.') + 1);
       String IntegerPart = binary.replace("." + FractionPart, "");
-      int num1 = 0;
+      long num1 = 0;
       char[] IntegeR = IntegerPart.toCharArray();
       int counter1 = 0;
       for (int i = IntegerPart.length() - 1; i >= 0; i--) {
@@ -15,7 +17,7 @@ public class Binary {
         num1 += (a * Math.pow(2, counter1));
         counter1++;
       }
-      String Dicemal1 = Integer.toString(num1);
+      String Dicemal1 = Long.toString(num1);
 
       Double num2 = 0.0;
       char[] FractioN = FractionPart.toCharArray();
@@ -30,7 +32,7 @@ public class Binary {
       return Dicemal;
     } else {
       String IntegerPart = binary;
-      int num1 = 0;
+      long num1 = 0;
       char[] IntegeR = IntegerPart.toCharArray();
       int counter1 = 0;
       for (int i = IntegerPart.length() - 1; i >= 0; i--) {
@@ -38,12 +40,10 @@ public class Binary {
         num1 += (a * Math.pow(2, counter1));
         counter1++;
       }
-      String Dicemal = Integer.toString(num1);
+      String Dicemal = Long.toString(num1);
       return Dicemal;
     }
   }
-
-
 
   public static String ToOctal(String binary) {
     if (binary.contains(".")) {
@@ -68,10 +68,10 @@ public class Binary {
         }
       }
 
-      int num1 = (Integer.parseInt(IntegerPart, 2));
-      String Octal1 = Integer.toOctalString(num1);
-      int num2 = (Integer.parseInt(FractionsPart, 2));
-      String Octal2 = Integer.toOctalString(num2);
+      long num1 = Long.parseLong(IntegerPart, 2);
+      String Octal1 = Long.toOctalString(num1);
+      long num2 = Long.parseLong(FractionsPart, 2);
+      String Octal2 = Long.toOctalString(num2);
 
       while (Octal2.charAt(Octal2.length() - 1) == '0') {
         Octal2 = removeLastCharacter(Octal2);
@@ -85,64 +85,68 @@ public class Binary {
         I = "0" + binary;
         binary = I;
       }
-      int num = (Integer.parseInt(binary, 2));
-      String Octal = Integer.toOctalString(num);
+      long num = Long.parseLong(binary, 2);
+      String Octal = Long.toOctalString(num);
       return Octal;
     }
   }
 
+  public static String removeLastCharacter(String str) {
+    if (str == null || str.isEmpty()) {
+      return str;
+    }
+    return str.substring(0, str.length() - 1);
+  }
+
+  // Helper method to remove the last character from a string
+
   public static String ToHexadecimal(String Binary) {
     if (Binary.contains(".")) {
       String FractionsPart = Binary.substring(Binary.indexOf('.') + 1);
-      String IntegerPart = Binary.replace("." + FractionsPart, "");
-      String I;
-      while (IntegerPart.length() % 4 != 0) {
-        I = "0" + IntegerPart;
-        IntegerPart = I;
-      }
-      while (FractionsPart.length() % 4 != 0) {
-        I = FractionsPart + "0";
-        FractionsPart = I;
-      }
+      String IntegerPart = Binary.substring(0, Binary.indexOf('.'));
 
-      String Zeros = "";
+      // Pad IntegerPart and FractionsPart to have a length multiple of 4
+      IntegerPart = padToMultipleOfFour(IntegerPart);
+      FractionsPart = padToMultipleOfFour(FractionsPart);
+
+      StringBuilder Zeros = new StringBuilder();
+
+      // Add zeros to Zeros for every group of 4 zeros at the beginning of
+      // FractionsPart
       int i = 0;
-      while (FractionsPart.substring(i, i + 4).equals("0000")) {
-        Zeros = Zeros.concat("0");
+      while (i < FractionsPart.length() && FractionsPart.substring(i, i + 4).equals("0000")) {
+        Zeros.append("0");
         i += 4;
-        if ((i + 4) > FractionsPart.length()) {
-          break;
-        }
-      }
-      int num1 = (Integer.parseInt(IntegerPart, 2));
-      String Hex1 = Integer.toHexString(num1);
-      int num2 = (Integer.parseInt(FractionsPart, 2));
-      String Hex2 = Integer.toHexString(num2);
-
-      while (Hex2.charAt(Hex2.length() - 1) == '0') {
-        Hex2 = removeLastCharacter(Hex2);
       }
 
-      String Hex = Hex1.toUpperCase() + "." + Zeros + Hex2.toUpperCase();
-      return Hex;
+      // Convert IntegerPart and FractionsPart to hexadecimal
+      String Hex1 = Long.toHexString(Long.parseLong(IntegerPart, 2)).toUpperCase();
+      String Hex2 = Long.toHexString(Long.parseLong(FractionsPart, 2)).toUpperCase();
+
+      // Remove trailing zeros from Hex2
+      while (!Hex2.isEmpty() && Hex2.charAt(Hex2.length() - 1) == '0') {
+        Hex2 = Hex2.substring(0, Hex2.length() - 1);
+      }
+
+      // Construct the final hexadecimal string
+      return Hex1 + "." + Zeros.toString() + Hex2;
     } else {
-      String I;
-      while (Binary.length() % 4 != 0) {
-        I = "0" + Binary;
-        Binary = I;
-      }
+      // Pad Binary to have a length multiple of 4
+      Binary = padToMultipleOfFour(Binary);
 
-      int num = (Integer.parseInt(Binary, 2));
-      String Hex = Integer.toHexString(num).toUpperCase();
+      // Convert Binary to hexadecimal
+      String Hex = Long.toHexString(Long.parseLong(Binary, 2)).toUpperCase();
+
       return Hex;
     }
   }
-  
-  private static String removeLastCharacter(String str) {
-    String result = null;
-    if ((str != null) && (str.length() > 0)) {
-       result = str.substring(0, str.length() - 1);
+
+  // Helper method to pad a binary string to have a length multiple of 4
+  private static String padToMultipleOfFour(String binary) {
+    while (binary.length() % 4 != 0) {
+      binary = "0" + binary;
     }
-    return result;
-}
+    return binary;
+  }
+
 }
